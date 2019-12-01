71d91b729f4e9393f0155da90814006f0fac3a66
nyan-svg <nyanlin1.1986@gmail.com>
2019-12-01 15:37:18 +0800

    Initial commit

---
+++ CurrencyExchangeSQLSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CurrencyExchangeSQLSample.Startup))]
+namespace CurrencyExchangeSQLSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
